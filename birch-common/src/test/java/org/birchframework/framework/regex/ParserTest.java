1bef765b5981c9b2e96fb55ebcd710a4eb56d267
Keivan Khalichi <keivan@khalichi.com>
2023-08-02 09:57:21 -0400

    Updated tests for parallel streams

--- birch-common/src/test/java/org/birchframework/framework/regex/ParserTest.java
+++ birch-common/src/test/java/org/birchframework/framework/regex/ParserTest.java
@@ -16,7 +16,10 @@ package org.birchframework.framework.regex;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import lombok.extern.java.Log;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -25,25 +28,47 @@ import static org.assertj.core.api.Assertions.*;
  * Tests for {@link Parser}.
  * @author Keivan Khalichi
  */
-public class ParserTest {
+@Log
+class ParserTest {
+
+   private final Path testFilePath = Paths.get("src/test/resources/parser-test-file.txt");
 
    /**
     * Tests {@link Parser#parse(String)}.
     */
    @Test
-   public void testExecute() throws IOException {
+   void testParse() throws IOException {
       final var aParser = Parser.of(TestParseClass.class);
       assertThat(aParser).isNotNull();
-      final var anInput = Files.readString(Paths.get("src/test/resources/parser-test-file.txt"));
+      final var anInput = Files.readString(this.testFilePath);
       assertThat(anInput).isNotBlank();
       final var anObjectsList = aParser.parse(anInput);
-      assertThat(anObjectsList).isNotNull();
-      assertThat(anObjectsList).isNotEmpty();
       assertThat(anObjectsList).size().isEqualTo(4);
       assertThat(anObjectsList.get(2)).isInstanceOf(TestParseClass.class);
       assertThat(((TestParseClass) anObjectsList.get(2)).getAge()).isEqualTo(33);
       assertThat(((TestParseClass) anObjectsList.get(2)).getLastName()).isEqualTo("Holden");
       assertThat(((TestParseClass) anObjectsList.get(1)).getType()).isEqualTo(TestParseClass.Type.ROCK_STAR);
       assertThat(((TestParseClass) anObjectsList.get(3)).getType()).isEqualTo(TestParseClass.Type.CHARACTER);
+   }
+
+   /**
+    * Tests {@link Parser#parse(java.util.stream.Stream)}.
+    */
+   @Test
+   void testParseLines() throws IOException {
+      final var aParser = Parser.of(TestParseClass.class);
+      assertThat(aParser).isNotNull();
+      try(final var aLines = Files.lines(this.testFilePath)) {
+         final var anObjectsStream = aParser.parse(aLines.parallel());
+         assertThat(anObjectsStream).isNotNull();
+         anObjectsStream.forEach(object -> {
+            assertThat(object).isNotNull();
+            assertThat(object).isInstanceOf(TestParseClass.class);
+            assertThat(((TestParseClass) object).getAge()).isPositive();
+            assertThat(((TestParseClass) object).getLastName()).isNotEmpty();
+            assertThat(((TestParseClass) object).getType()).isNotNull();
+            log.info(ToStringBuilder.reflectionToString(object));
+         });
+      }
    }
 }
